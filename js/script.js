70abd6efb52f6566194f87e6b262ba6631d00ca9
Vladislav-1 <44432412+Vladislav-1@users.noreply.github.com>
2024-11-05 16:47:54 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
+function hide_link_vk() {
+    document.getElementById('vk_on').style = "display: none;";
+    document.getElementById('vk_off').style = "display: inline-block;";
+}
 
+function show_link_vk() {
+    document.getElementById('vk_on').style = "display: inline-block;";
+    document.getElementById('vk_off').style = "display: none;";
+}
+
+function hide_link_telegram() {
+    document.getElementById('telegram_on').style = "display: none;";
+    document.getElementById('telegram_off').style = "display: inline-block;";
+}
+
+function show_link_telegram() {
+    document.getElementById('telegram_on').style = "display: inline-block;";
+    document.getElementById('telegram_off').style = "display: none;";
+}
+
+function hide_link_dzen() {
+    document.getElementById('dzen_on').style = "display: none;";
+    document.getElementById('dzen_off').style = "display: inline-block;";
+}
+
+function show_link_dzen() {
+    document.getElementById('dzen_on').style = "display: inline-block;";
+    document.getElementById('dzen_off').style = "display: none;";
+}
