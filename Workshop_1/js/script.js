2920f5c5e22e148ab9520bb1026f1d835a9c0374
Vladislav-1 <44432412+Vladislav-1@users.noreply.github.com>
2024-11-22 11:37:53 +0500

    Update script.js

--- Workshop_1/js/script.js
+++ Workshop_1/js/script.js
@@ -1,1 +1,117 @@
+function show_icons_0_5() {
+    document.getElementById('main__card-TATRAN-icon-left').style = "opacity: 0.5;";
+    document.getElementById('main__card-TATRAN-icon-right').style = "opacity: 0.5;";
+}
 
+function show_icons_1_0_left() {
+    document.getElementById('main__card-TATRAN-icon-left').style = "opacity: 1.0;";
+}
+
+function hide_icons_left() {
+    document.getElementById('main__card-TATRAN-icon-left').style = "opacity: 0.5;";
+}
+
+function show_icons_1_0_right() {
+    document.getElementById('main__card-TATRAN-icon-right').style = "opacity: 1.0;";
+}
+
+function hide_icons_right() {
+    document.getElementById('main__card-TATRAN-icon-right').style = "opacity: 0.5;";
+}
+
+function hide_icons() {
+    document.getElementById('main__card-TATRAN-icon-left').style = "opacity: 0.0;";
+    document.getElementById('main__card-TATRAN-icon-right').style = "opacity: 0.0;";
+}
+
+function show_rectangle_1() {
+    document.getElementById('rectangle_1_1').style = "opacity: 0.5;";
+    document.getElementById('rectangle_1_2').style = "opacity: 0.5;";
+}
+
+function show_show_rectangle_1_1() {
+    document.getElementById('rectangle_1_1').style = "opacity: 1.0;";
+}
+
+function hiden_rectangle_1_1() {
+    document.getElementById('rectangle_1_1').style = "opacity: 0.5;";
+}
+
+function show_show_rectangle_1_2() {
+    document.getElementById('rectangle_1_2').style = "opacity: 1.0;";
+}
+
+function hiden_rectangle_1_2() {
+    document.getElementById('rectangle_1_2').style = "opacity: 0.5;";
+}
+
+function hiden_rectangle_1() {
+    document.getElementById('rectangle_1_1').style = "opacity: 0.0;";
+    document.getElementById('rectangle_1_2').style = "opacity: 0.0;";
+}
+
+/* ----------------------------- */
+
+function show_rectangle_2() {
+    document.getElementById('rectangle_2_1').style = "opacity: 0.5;";
+    document.getElementById('rectangle_2_2').style = "opacity: 0.5;";
+}
+
+function show_show_rectangle_2_1() {
+    document.getElementById('rectangle_2_1').style = "opacity: 1.0;";
+}
+
+function hiden_rectangle_2_1() {
+    document.getElementById('rectangle_2_1').style = "opacity: 0.5;";
+}
+
+function show_show_rectangle_2_2() {
+    document.getElementById('rectangle_2_2').style = "opacity: 1.0;";
+}
+
+function hiden_rectangle_2_2() {
+    document.getElementById('rectangle_2_2').style = "opacity: 0.5;";
+}
+
+function hiden_rectangle_2() {
+    document.getElementById('rectangle_2_1').style = "opacity: 0.0;";
+    document.getElementById('rectangle_2_2').style = "opacity: 0.0;";
+}
+
+/* ----------------------------- */
+
+function show_rectangle_3() {
+    document.getElementById('rectangle_3_1').style = "opacity: 0.5;";
+    document.getElementById('rectangle_3_2').style = "opacity: 0.5;";
+}
+
+function show_show_rectangle_3_1() {
+    document.getElementById('rectangle_3_1').style = "opacity: 1.0;";
+}
+
+function hiden_rectangle_3_1() {
+    document.getElementById('rectangle_3_1').style = "opacity: 0.5;";
+}
+
+function show_show_rectangle_3_2() {
+    document.getElementById('rectangle_3_2').style = "opacity: 1.0;";
+}
+
+function hiden_rectangle_3_2() {
+    document.getElementById('rectangle_3_2').style = "opacity: 0.5;";
+}
+
+function hiden_rectangle_3() {
+    document.getElementById('rectangle_3_1').style = "opacity: 0.0;";
+    document.getElementById('rectangle_3_2').style = "opacity: 0.0;";
+}
+
+/* ----------------------------- */
+
+function showFiltr() {
+    document.getElementById('main__form').style = "display: flex;";
+}
+
+function hideFiltr() {
+    document.getElementById('main__form').style = "display: none;";
+}
